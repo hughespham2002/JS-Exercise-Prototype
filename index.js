4ee24ce3cb4624a42c2113f5c4624bab3f5543c2
Hughes <hughespham2002the2nd@yahoo.com>
2021-09-15 19:31:15 -0500

    3/4

--- index.js
+++ index.js
@@ -39,14 +39,24 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
+Person.prototype.eat = function(edible) {
+  if(this.stomach.length < 10) 
+    this.stomach.push(edible);
+}
+Person.prototype.poop = function() {
+  this.stomach = [];
+}
+Person.prototype.toString = function() {
+
+  return `${this.name}, ${this.age}`;
 }
 
-
-
-
-
+const neo = new Person("Neo", 20);
 
 
 /*
@@ -63,9 +73,16 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons;
+};
 
 
 /*
@@ -75,15 +92,19 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+ this.favoriteToy = favoriteToy;
+ Person.call(this, name, age)
 }
-
+Baby.prototype = Object.create(Person.prototype)
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`;
+}
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
+  1. this in method returns an object but in functions returns global objects
   2. 
   3. 
   4. 
